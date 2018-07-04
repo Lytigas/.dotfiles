162a871f536ade141971844f7906fbf4f4580f1d
Josh Hejna <josh.hejna@gmail.com>
2018-07-04 01:04:18 -0700

    Don't repeat install

--- install.sh
+++ install.sh
@@ -2,31 +2,9 @@
 
 set -ea
 
-DOTFILES_DIR="$PWD"
-INST_SCRIPT="install.sh"
-
 source ./include.sh
 
-function install_target() {
-	if [[ "$1" =~ "no_target" ]]; then
-		return
-	fi
-	if [ ! -d "$1" ]; then
-		echo "Could not find directory '$1', skipping."
-		return
-	fi
-	if [ ! -f "$1/$INST_SCRIPT" ]; then
-		echo "Could not run '$1/$INST_SCRIPT', file not found."
-		return
-	fi
-	echo "Running install.sh for $1"
-	cd "$1" && chmod +x "$INST_SCRIPT" && ./$INST_SCRIPT
-}
-# export for xargs
-export -f install_target
-
-# Change to the dir of this script
-cd "${0%/*}"
+cd "$DOTFILE_ROOT"
 
 if [ "$#" -lt 1 ]; then
 	echo "Provide at least one target"
@@ -34,7 +12,7 @@ if [ "$#" -lt 1 ]; then
 fi
 
 if [ "$1" = "all" ]; then
-	for target in $(ls -d -- */)
+	for target in $(ls -1 -d -- */)
 	do
 		install_target "$target"
 	done
