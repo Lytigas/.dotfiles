12f04219c8eab5fbcbae85be5bb21b515ddde621
Josh Hejna <josh.hejna@gmail.com>
2018-07-04 22:19:01 -0700

    Add vscode extensions and keybindings

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,11 +2,23 @@
 set -e
 
 # verify vscode is installed
-command -v code || exit 1
+command -v code || {
+    echo "vscode not installed, skipping..."
+    exit 1
+}
 
-# icons
+echo "Installing vscode extensions..."
+
+# html
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.auto-close-tag
+# md
+code --install-extension DavidAnson.vscode-markdownlint
+# general
+code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension robertohuertasm.vscode-icons
 
-# symlink settings
+echo "Symlinking vscode configs"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ask_make_link "$DIR/settings.json" "$HOME/.config/Code/User/settings.json"
+ask_make_link "$DIR/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
