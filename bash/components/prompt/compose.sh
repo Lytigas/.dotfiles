4525ad011b8af21c6d63db6d23490c7b71ba74f0
Josh Hejna <josh.hejna@gmail.com>
2018-07-31 10:07:19 -0700

    Don't append git output to .bashrc

--- bash/components/prompt/compose.sh
+++ bash/components/prompt/compose.sh
@@ -3,6 +3,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-git submodule update --init --recursive
+git submodule update --init --recursive > /dev/null 
 echo '# Include liquidprompt'
 echo "source $DIR/liquidprompt/liquidprompt"
