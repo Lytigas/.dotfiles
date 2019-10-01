09109a7750754af4f130ad2ee8c8275cab908215
Josh Hejna <josh.hejna@gmail.com>
2019-09-30 22:45:42 -0700

    Add epen alias

--- bash/components/aliases/aliases.sh
+++ bash/components/aliases/aliases.sh
@@ -8,3 +8,6 @@ up() { cd $(eval printf '../'%.0s {1..$1}) && pwd; }
 
 # xdg-open shortcut, openvt is almost always also /bin/openvt
 alias open='xdg-open'
+function epen {
+    open "$1" && exit
+}
